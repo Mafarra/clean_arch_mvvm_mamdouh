66c54dfdc744227268a2502894a7ba20d3c2e940
Mammdouh Al-farra <mam.farra2030@gmail.com>
2023-09-16 15:35:30 +0300

    finish: manager_theme.dart :inputDecorationTheme

--- lib/presentaion/manager_theme.dart
+++ lib/presentaion/manager_theme.dart
@@ -53,15 +53,58 @@ ThemeData getApplicationTheme() {
 
     // text theme
     textTheme: TextTheme(
-      displayLarge: getSemiBoldStyle(
-          color: ManagerColor.white, fontSize: FontSize.s22),
-        headlineLarge: getSemiBoldStyle(
-            color: ManagerColor.darkGrey, fontSize: FontSize.s16),
-        titleMedium:  getMediumStyle(color: ManagerColor.lightGrey,fontSize: FontSize.s14),
-        bodySmall: getRegularStyle(color: ManagerColor.grey1),
-        bodyLarge: getRegularStyle(color: ManagerColor.grey),
+      displayLarge:
+          getSemiBoldStyle(color: ManagerColor.white, fontSize: FontSize.s22),
+      headlineLarge: getSemiBoldStyle(
+          color: ManagerColor.darkGrey, fontSize: FontSize.s16),
+      titleMedium:
+          getMediumStyle(color: ManagerColor.lightGrey, fontSize: FontSize.s14),
+      bodySmall: getRegularStyle(color: ManagerColor.grey1),
+      bodyLarge: getRegularStyle(color: ManagerColor.grey),
     ),
 
     //input decoration theme (text for field)
+    inputDecorationTheme: InputDecorationTheme(
+      //content padding
+      contentPadding: const EdgeInsets.all(AppPadding.p8),
+      //hint styhle
+      hintStyle:
+          getRegularStyle(color: ManagerColor.grey, fontSize: FontSize.s14),
+      labelStyle:
+          getMediumStyle(color: ManagerColor.grey, fontSize: FontSize.s14),
+      errorStyle: getRegularStyle(color: ManagerColor.error),
+
+      //enabled border style
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: ManagerColor.grey, width: AppSize.s1_5),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(AppSize.s8),
+        ),
+      ),
+
+      //focused border style
+      focusedBorder: OutlineInputBorder(
+        borderSide:
+            BorderSide(color: ManagerColor.primary, width: AppSize.s1_5),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(AppSize.s8),
+        ),
+      ),
+      //error border style
+      errorBorder: OutlineInputBorder(
+        borderSide:
+            BorderSide(color: ManagerColor.error, width: AppSize.s1_5),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(AppSize.s8),
+        ),
+      ),
+      focusedErrorBorder: OutlineInputBorder(
+        borderSide:
+        BorderSide(color: ManagerColor.primary, width: AppSize.s1_5),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(AppSize.s8),
+        ),
+      ),
+    ),
   );
 }
