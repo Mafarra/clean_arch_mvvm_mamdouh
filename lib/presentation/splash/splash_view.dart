b831fbda17a8763ee00a071f7c3015765b8c5f59
Mammdouh Al-farra <mam.farra2030@gmail.com>
2023-09-23 15:12:23 +0300

    add timer,_goNext(),initState(),dispose()

--- lib/presentation/splash/splash_view.dart
+++ lib/presentation/splash/splash_view.dart
@@ -1,7 +1,8 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-
 import '../resources/manager_assets.dart';
 import '../resources/manager_color.dart';
+import '../resources/manager_routes.dart';
 
 class SplashView extends StatefulWidget {
   const SplashView({Key? key}) : super(key: key);
@@ -11,11 +12,29 @@ class SplashView extends StatefulWidget {
 }
 
 class _SplashViewState extends State<SplashView> {
+  Timer? _timer;
+
+  _startDelay(){
+    _timer = Timer(const Duration(seconds: 3),_goNext);
+  }
+  _goNext(){
+    Navigator.pushReplacementNamed(context, Routes.onBoardingRoute);
+  }
+  @override
+  void initState() {
+    super.initState();
+    _startDelay();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ManagerColor.primary,
       body: const Center(child: Image(image: AssetImage(ImageAssets.splshLogo),)),
     );
+  }
+  @override
+  void dispose() {
+    _timer!.isActive?_timer?.cancel():_timer;
+    super.dispose();
   }
 }
